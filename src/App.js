abaad6baf4c36642b2a216781f772c5ba37b720c
Vinu-1103 <vinuchandra1103@gmail.com>
2024-06-18 11:11:11 +0530

    HELLO

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  //const value = 'World';
+ // return <div>Hello {value}</div>;
+  return <div><h1>THIS IS MY FIRST STATIV WEB APP<H1/></div>;
+
 }
 
 export default App;
